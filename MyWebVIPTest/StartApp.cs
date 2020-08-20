daf57a482c560f91152d3596bee40f67389cbd78
Jason Branham <jbranham@ranorex.com>
2020-08-20 12:47:57 -0400

    added new actions to StartApp

--- MyWebVIPTest/StartApp.cs
+++ MyWebVIPTest/StartApp.cs
@@ -83,6 +83,9 @@ namespace MyWebVIPTest
             Host.Current.OpenBrowser("http://www.ranorex.com/web-testing-examples/vip/", "IE", "", false, false, false, false, false, true);
             Delay.Milliseconds(0);
             
+            // added action
+            Report.Log(ReportLevel.Info, "Section", "added action", new RecordItemIndex(1));
+            
         }
 
 #region Image Feature Data
